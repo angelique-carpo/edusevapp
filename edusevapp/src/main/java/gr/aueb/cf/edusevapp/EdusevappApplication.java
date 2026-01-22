5c2efa989d020e13ae3bea06a7afd50def3488da
Seedswirl <angelique.carpo@gmail.com>
2026-01-22 15:28:38 +0200

    Initial commit

---
+++ edusevapp/src/main/java/gr/aueb/cf/edusevapp/EdusevappApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.edusevapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdusevappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdusevappApplication.class, args);
+	}
+
+}
---
+++ edusevapp/src/test/java/gr/aueb/cf/edusevapp/EdusevappApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.edusevapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdusevappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
